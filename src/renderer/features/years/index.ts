330d1e2677bc575e2bee36ce9d5ea9d72c8da89c
Dima Markus <dmi3000@gmail.com>
2025-06-11 18:40:29 -0400

    Fix index file

--- src/renderer/features/years/index.ts
+++ src/renderer/features/years/index.ts
@@ -1,14 +1,18 @@
+export { OptimizedYearAlbumMosaic } from './components/optimized-year-album-mosaic';
 export { YearAlbumListFilters } from './components/year-album-list-filters';
+export { YearAlbumMosaic } from './components/year-album-mosaic';
 export { YearDetailHeader } from './components/year-detail-header';
 export { YearNavigation } from './components/year-navigation';
 export { YearsDecadeRoute } from './components/years-decade-route';
 export { YearsListContent } from './components/years-list-content';
 export { YearsListGridView } from './components/years-list-grid-view';
-export { YearsListHeader } from './components/years-list-header';
-// New list view components
-export { default as YearsListRoute } from './components/years-list-route';
+export { YearsListHeaderFilters } from './components/years-list-header-filters';
 export { YearsListTableView } from './components/years-list-table-view';
-export { YearsOverviewRoute } from './components/years-overview-route';
 export { YearsPlaylistRoute } from './components/years-playlist-route';
+
+export { useOptimizedYearAlbumCounts } from './hooks/use-optimized-year-album-counts';
+export { useProcessedYears } from './hooks/use-processed-years';
 export { useYearAlbumCounts } from './hooks/use-year-album-counts';
-export * from './years-playlists';
+
+export { YEAR_DECADES, YEAR_INDIVIDUALS, YEAR_PLAYLISTS } from './years-playlists';
+export type { YearPlaylist } from './years-playlists';
